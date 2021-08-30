037e6a9df0b79a81408c92e76d80c55fd5375f03
Unknown <lilrwatty@gmail.com>
2021-08-30 19:04:48 -0400

    Initial commit

---
+++ RandomQuote/Data/Quote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RandomQuote.Data
+{
+    public class QuoteResponse
+    {
+        public Quote Quote { get; set; }
+    }
+
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+    }
+}
